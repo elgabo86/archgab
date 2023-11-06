a937daacdcd30f681a6edc58c1868d4ef54296e6
Gab <gabriel.tronchoni@gmail.com>
2023-11-06 16:01:07 +0100

    update

--- customperso.sh
+++ customperso.sh
@@ -7,3 +7,14 @@ fi
 
 # fr_CH fix
 export LANG=fr_CH.UTF-8
+
+# alias
+
+alias goyou="ytfzf"
+alias goyou-th="ytfzf -t -T chafa"
+alias goody="ytfzf -cO"
+alias goody-th="ytfzf -t -T chafa -cO"
+alias goody-nsfw="ytfzf --nsfw -cO"
+alias goody-nsfw-th="ytfzf -t -T chafa --nsfw -cO"
+alias gopee="ytfzf -cP"
+alias gopee-th="ytfzf -t -T chafa -cP"
